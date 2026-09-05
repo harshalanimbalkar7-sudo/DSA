8007c70174ffe5c4e2a0757e60f37a11af824b5b
harshalanimbalkar7-sudo <harshalanimbalkar7@gmail.com>
2026-09-05 18:47:48 +0530

    Add files via upload

--- string.cpp
+++ string.cpp
@@ -344,3 +344,30 @@ string reverseWords(string s) {
 
     return ans;
 }
+
+#include <bits/stdc++.h>
+using namespace std;
+
+string removeOuterParentheses(string s) {
+
+    string ans;
+    int depth = 0;
+
+    for (char c : s) {
+
+        if (c == '(') {
+            if (depth > 0)
+                ans += c;
+
+            depth++;
+        }
+        else {
+            depth--;
+
+            if (depth > 0)
+                ans += c;
+        }
+    }
+
+    return ans;
+}
