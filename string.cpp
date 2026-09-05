348eacc9a2adcbffb529c51a0a2db2cef79485ed
harshalanimbalkar7-sudo <harshalanimbalkar7@gmail.com>
2026-09-05 18:46:51 +0530

    Add files via upload

--- string.cpp
+++ string.cpp
@@ -316,3 +316,31 @@ int lengthOfLongestSubstring(string s) {
 
     return ans;
 }
+
+#include <bits/stdc++.h>
+using namespace std;
+
+string reverseWords(string s) {
+
+    stringstream ss(s);
+
+    vector<string> words;
+    string word;
+
+    while (ss >> word)
+        words.push_back(word);
+
+    reverse(words.begin(), words.end());
+
+    string ans;
+
+    for (int i = 0; i < words.size(); i++) {
+
+        if (i > 0)
+            ans += " ";
+
+        ans += words[i];
+    }
+
+    return ans;
+}
