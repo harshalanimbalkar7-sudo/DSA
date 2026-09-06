155267f21450f4987c5cfbb7074dba79cbf63f51
harshalanimbalkar7-sudo <harshalanimbalkar7@gmail.com>
2026-09-06 23:48:36 +0530

    Add files via upload

--- string.cpp
+++ string.cpp
@@ -435,3 +435,29 @@ int compress(vector<char>& chars) {
 
     return write;
 }
+
+#include <bits/stdc++.h>
+using namespace std;
+
+string frequencySort(string s) {
+
+    unordered_map<char, int> freq;
+
+    for (char c : s)
+        freq[c]++;
+
+    vector<pair<char, int>> v(freq.begin(), freq.end());
+
+    sort(v.begin(), v.end(),
+         [](auto &a, auto &b) {
+             return a.second > b.second;
+         });
+
+    string ans;
+
+    for (auto &p : v) {
+        ans.append(p.second, p.first);
+    }
+
+    return ans;
+}
