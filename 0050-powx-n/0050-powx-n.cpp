3caf4944b0d7075b7d35cddf61a53d606dc5e0a8
harshalanimbalkar7-sudo <harshalanimbalkar7@gmail.com>
2026-09-21 13:46:45 +0530

    Time: 0 ms (100%), Space: 8.6 MB (44.16%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        
+        long long power = n;
+
+        // Handle negative power
+        if (power < 0) {
+            x = 1 / x;
+            power = -power;
+        }
+
+        double ans = 1.0;
+
+        while (power > 0) {
+            
+            // If power is odd
+            if (power % 2 == 1) {
+                ans = ans * x;
+            }
+
+            // Square x
+            x = x * x;
+
+            // Divide power by 2
+            power = power / 2;
+        }
+
+        return ans;
+    }
+};
